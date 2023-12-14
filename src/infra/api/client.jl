50d27e3684a3b0b5aab44ba75e74474c7805d3e7
tau <36781951+tndd@users.noreply.github.com>
2023-12-15 00:08:10 +0900

    refactor client
    Api side should not know format of the db.

--- src/infra/api/client.jl
+++ src/infra/api/client.jl
@@ -3,55 +3,33 @@ using HTTP
 
 DotEnv.config()
 
-struct ApiRequest
+struct ApiResponse
     url::String
     query::Dict
     header::Dict
+    r_status::Int
+    r_header::Dict
+    r_body::Dict
 end
 
 
-struct ApiResponse
-    status::Int
-    header::Dict
-    body::Dict
-end
-
-
-struct ApiSnapshot
-    session_id::Union{Int, Nothing}
-    seq_n::Union{Int, Nothing}
-    req::ApiRequest
-    res::ApiResponse
-end
-
-
-function req_get(
-        url::String,
-        query::Dict,
-        headers::Dict,
-        session_id=Nothing,
-        seq_n=Nothing
-    )::ApiSnapshot
-    # request
-    r = HTTP.get(url, query=query, headers=headers)
-    # response
-    r_status::Int = r.status
-    r_headers::Dict = Dict(r.headers)
-    r_body::String = String(r.body)
-    # store
-    return ApiSnapshot(
-        session_id,
-        seq_n,
-        ApiRequest(url, query, headers),
-        ApiResponse(r_status, r_headers, r_body)
+function r_get(url::String, query::Dict, header::Dict)::ApiResponse
+    r = HTTP.get(url, query=query, headers=header)
+    return ApiResponse(
+        url,
+        query,
+        header,
+        r.status,
+        Dict(r.headers),
+        String(r.body)
     )
 end
 
 
-function req_get_as_alpaca(url::String, query::Dict)
+function r_get_as_alpaca(url::String, query::Dict)::ApiResponse
     header = Dict(
         "APCA-API-KEY-ID" => ENV["APCA_API_KEY_ID"],
         "APCA-API-SECRET-KEY" => ENV["APCA_API_SECRET_KEY"]
     )
-    req_get(url, query, header)
+    return r_get(url, query, header)
 end
