14775213474c08fe387211765ad0c8612cf8326c
tau <36781951+tndd@users.noreply.github.com>
2023-12-14 23:58:49 +0900

    upd client

--- src/infra/api/client.jl
+++ src/infra/api/client.jl
@@ -1,4 +1,7 @@
+using DotEnv
 using HTTP
+
+DotEnv.config()
 
 struct ApiRequest
     url::String
@@ -6,20 +9,29 @@ struct ApiRequest
     header::Dict
 end
 
+
 struct ApiResponse
     status::Int
     header::Dict
     body::Dict
 end
 
+
 struct ApiSnapshot
-    session_id::Int
-    seq_n::Int
+    session_id::Union{Int, Nothing}
+    seq_n::Union{Int, Nothing}
     req::ApiRequest
     res::ApiResponse
 end
 
-function req_get(url::String, query::Dict, headers::Dict)
+
+function req_get(
+        url::String,
+        query::Dict,
+        headers::Dict,
+        session_id=Nothing,
+        seq_n=Nothing
+    )::ApiSnapshot
     # request
     r = HTTP.get(url, query=query, headers=headers)
     # response
@@ -27,5 +39,19 @@ function req_get(url::String, query::Dict, headers::Dict)
     r_headers::Dict = Dict(r.headers)
     r_body::String = String(r.body)
     # store
-    return r_body
+    return ApiSnapshot(
+        session_id,
+        seq_n,
+        ApiRequest(url, query, headers),
+        ApiResponse(r_status, r_headers, r_body)
+    )
+end
+
+
+function req_get_as_alpaca(url::String, query::Dict)
+    header = Dict(
+        "APCA-API-KEY-ID" => ENV["APCA_API_KEY_ID"],
+        "APCA-API-SECRET-KEY" => ENV["APCA_API_SECRET_KEY"]
+    )
+    req_get(url, query, header)
 end
