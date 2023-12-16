36d564e7c7b5ab2ddf22400a2968731793ea25b9
tau <36781951+tndd@users.noreply.github.com>
2023-12-17 04:46:41 +0900

    upd table def

--- src/infra/db/postgres/sql/dataflow/select/todo_api.sql
+++ src/infra/db/postgres/sql/dataflow/select/todo_api.sql
@@ -1,5 +1,5 @@
 WITH max_timestamps_snapshot AS (
-  SELECT api_schedule_id, MAX(timestamp) AS max_timestamp
+  SELECT api_schedule_id, MAX(time_stamp) AS max_timestamp
   FROM api_snapshot
   GROUP BY api_schedule_id
 ),
@@ -8,11 +8,10 @@ latest_snapshots AS (
   FROM api_snapshot s
   JOIN max_timestamps_snapshot mts
     ON s.api_schedule_id = mts.api_schedule_id
-   AND s.timestamp = mts.max_timestamp
+   AND s.time_stamp = mts.max_timestamp
 )
 SELECT *
 FROM api_schedule sch
 LEFT JOIN latest_snapshots lt_s
   ON sch.id = lt_s.api_schedule_id
-WHERE sch.is_completed = false
-  AND (lt_s.status IS NULL OR lt_s.status <> 200);
+WHERE lt_s.status IS NULL OR lt_s.status <> 200;
