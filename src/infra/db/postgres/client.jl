cfa4e0873a8c35aefaf8d4c254a25e09d8db40f4
tau <36781951+tndd@users.noreply.github.com>
2023-12-17 21:55:29 +0900

    why need . on Thread head??

--- src/infra/db/postgres/client.jl
+++ src/infra/db/postgres/client.jl
@@ -1,13 +1,13 @@
+using .Threads
 using DotEnv
 using LibPQ
-
 
 DotEnv.config()
 
 
 function transact_execute(queries::Vector{String})
-    conn = LibPQ.Connection(ENV["PSQL_URL"])
     try
+        conn = LibPQ.Connection(ENV["PSQL_URL"])
         LibPQ.begin(conn)
         for query in queries
             execute(conn, query)
